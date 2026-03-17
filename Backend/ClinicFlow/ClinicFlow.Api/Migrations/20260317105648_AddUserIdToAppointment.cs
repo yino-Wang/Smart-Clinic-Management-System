524490ea908b4160784469d6aa5bbf4269bdff15
yino-Wang <yinooo66688@gmail.com>
2026-03-17 22:11:56 +1100

    fixed some bugs

---
+++ Backend/ClinicFlow/ClinicFlow.Api/Migrations/20260317105648_AddUserIdToAppointment.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicFlow.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Appointments",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_UserAccounts_UserId",
+                table: "Appointments",
+                column: "UserId",
+                principalTable: "UserAccounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_UserAccounts_UserId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Appointments");
+        }
+    }
+}
